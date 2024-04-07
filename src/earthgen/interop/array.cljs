d9e5f14029ba2981105875ebceef266c2c35ff09
Tobias Grelsson <tgrelsson@gmail.com>
2024-04-07 22:58:07 +0200

    js array interop utils

---
+++ src/earthgen/interop/array.cljs
@@ -0,0 +1,23 @@
+(ns earthgen.interop.array
+  (:refer-clojure :exclude [get map reduce concat count take drop]))
+
+(def get aget)
+
+(defn map
+  ([f a] (amap a n _ (f (aget a n))))
+  ([f a b] (amap a n _ (f (aget a n) (aget b n)))))
+
+(defn reduce [f init a]
+  (areduce a n accum init (f accum (aget a n))))
+
+(defn concat
+  ([a b] (.concat a b)))
+
+(defn count [a]
+  (alength a))
+
+(defn take [n arr]
+  (.slice arr 0 n))
+
+(defn drop [n arr]
+  (.slice arr n (count arr)))
