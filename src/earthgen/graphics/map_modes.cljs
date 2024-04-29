e92bf7991afa34d6ae2e3a41ee4fb180431d7704
Tobias Grelsson <tgrelsson@gmail.com>
2024-04-29 21:43:07 +0200

    add solar intensity, refactor map modes

--- src/earthgen/graphics/map_modes.cljs
+++ src/earthgen/graphics/map_modes.cljs
@@ -3,13 +3,16 @@
 
 (defn in-bands [bands]
   (let
-   [stop-at (- (js-array/count bands) 2)]
-    (fn [a]
-      (loop [n 0]
-        (if (or (= n stop-at)
-                (<= a (js-array/get bands (inc n))))
-          n
-          (recur (inc n)))))))
+   [[min-value max-value] [(js-array/first bands) (js-array/last bands)]
+    stop-at (- (js-array/count bands) 2)]
+    (fn [value]
+      (let
+       [a (max min-value (min max-value value))]
+        (loop [n 0]
+          (if (or (= n stop-at)
+                  (<= a (js-array/get bands (inc n))))
+            n
+            (recur (inc n))))))))
 
 (defn elevation [planet]
   (let
@@ -24,17 +27,29 @@
                      [0.93 0.7 0.3 1]
                      [0.8 0.5 0.15 1]
                      [0.4 0.25 0 1]])
-    [min-value max-value] [(js-array/first bands) (js-array/last bands)]
     sea-level (:sea-level planet)
     to-value (fn [a]
                (- a sea-level))
     elevation-in (fn [vec]
                    #(->> %
                          (js-array/get vec)
-                         to-value))
-    get-band (in-bands bands)
-    color #(->> %
-                (max min-value)
-                (min max-value)
-                get-band)]
-    [colors (comp color (elevation-in (:tile-elevation planet)))]))
+                         to-value))]
+    [colors (comp (in-bands bands) (elevation-in (:tile-elevation planet)))]))
+
+(defn solar-intensity [planet]
+  (let
+   [bands #js [0 0 0.1 0.2 0.3 0.4 0.5 0.6 0.7 0.8 0.9 1.0]
+    colors (clj->js [[1 1 1 1]
+                     [1 1 0 1]
+                     [1 0.8 0 1]
+                     [1 0.6 0 1]
+                     [1 0.4 0 1]
+                     [1 0.2 0 1]
+                     [1 0 0 1]
+                     [0.8 0 0 1]
+                     [0.6 0 0 1]
+                     [0.4 0 0 1]
+                     [0.2 0 0 1]])
+    get-in (fn [vec]
+             #(js-array/get vec %))]
+    [colors (comp (in-bands bands) (get-in (:potential-solar-radiation planet)))]))
